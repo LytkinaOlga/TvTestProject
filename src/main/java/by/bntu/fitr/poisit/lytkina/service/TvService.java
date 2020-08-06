9a0f1c0be028299cff7c64943acfac0c9620cb69
LytkinaOlga <65194987+LytkinaOlga@users.noreply.github.com>
2020-08-07 00:00:23 +0300

    create test project with sonar, jacoco, logger and junit tests with gitignore

--- src/main/java/by/bntu/fitr/poisit/lytkina/service/TvService.java
+++ src/main/java/by/bntu/fitr/poisit/lytkina/service/TvService.java
@@ -13,7 +13,7 @@ public class TvService {
     }
 
     public boolean ifOne(){
-        logger.info("checking if the TV is  on");
+        logger.info("checking if the TV is on (in trunk)");
         return tv.isOn();
     }
 }
